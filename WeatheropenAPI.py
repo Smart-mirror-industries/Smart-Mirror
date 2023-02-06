bdac32c62c7193334a85cc8568a8c94a09393bdf
b0b00646 <h8jazz@gmail.com>
2023-02-06 12:42:14 -0500

    Update WeatheropenAPI.py
    More changes, just getting lat and lon, as well as adding an intial start for weathergrab

--- WeatheropenAPI.py
+++ WeatheropenAPI.py
@@ -1,13 +1,28 @@
-
+import requests
 
 #This will be how the system gathers weather data
 
 #Just grab zipcode first
 def Latlon(zpcode):
     print("Zipcode: " +zpcode) #Why in the everloving EARTH do I have to define it like this first.
+
+    Location = f"https://nominatim.openstreetmap.org/search?q={zpcode}&format=json"
+    response = requests.get(Location)
+    data = response.json()#Reading the JSON
+    lat = data[0]['lat'] #grabbing ze data
+    lon = data[0]['lon']
+    return lat, lon
+
+#Now for the weather, maybe eventually
+def weathergrab(lat, lon):
+    #bazinga
+    return
     
 
 zipcode = input("Input your zipcode:" ) #I didn't know you could shorthand this
 
-Latlon(zipcode)
+lat, lon = Latlon(zipcode)
 
+print(f"Lat: {lat}, Lon: {lon}")
+
+weathergrab(lat, lon)
