4aeaef2d33b109279fba215802e7c3a458a3762f
b0b00646 <h8jazz@gmail.com>
2023-02-06 11:48:25 -0500

    Create WeatheropenAPI.py
    Just the inital work for it

---
+++ WeatheropenAPI.py
@@ -0,0 +1,13 @@
+
+
+#This will be how the system gathers weather data
+
+#Just grab zipcode first
+def Latlon(zpcode):
+    print("Zipcode: " +zpcode) #Why in the everloving EARTH do I have to define it like this first.
+    
+
+zipcode = input("Input your zipcode:" ) #I didn't know you could shorthand this
+
+Latlon(zipcode)
+
