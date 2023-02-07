1c8c1b6bc5cdcca1636294d36d7ad56484e3264f
b0b00646 <h8jazz@gmail.com>
2023-02-07 16:17:13 -0500

    Update WeatheropenAPI.py
    Added the rest of the weathergrab to get today, and the next two days, expansion into that could be easy.

--- WeatheropenAPI.py
+++ WeatheropenAPI.py
@@ -9,7 +9,7 @@ import json
 def Latlon(zpcode):
     print("Zipcode: " +zpcode) #Why in the everloving EARTH do I have to define it like this first.
 
-    Location = f"https://nominatim.openstreetmap.org/search?q={zpcode}&format=json"
+    Location = f"https://nominatim.openstreetmap.org/search?q={zpcode}&format=json" #fstrings are really neat
     response = requests.get(Location)
     data = response.json()#Reading the JSON
     lat = data[0]['lat'] #grabbing ze data
@@ -17,18 +17,18 @@ def Latlon(zpcode):
     return lat, lon
 
 #Now for the weather, maybe eventually
-def weathergrab(lat, lon):
+def weathergrab(lat, lon, set):
     #bazinga
     weather = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=imperial&exclude=hourly,alerts,minutely&appid=dbfe113373f8e233af2191ce8daf6a90"
    #call complete ring ring ring
     response = requests.get(weather)
     data = response.json()
-    date1 = data['daily'][0]['dt']
-    readdate = datetime.datetime.fromtimestamp(date1).strftime('%Y-%m-%d')
-    low1 = data['daily'][0]['temp']['min']
-    high1 = data['daily'][0]['temp']['max'] #I think this is right?
-    #descrip = data['daily'][0]['weather']['0']
-    return readdate, low1, high1
+    date1 = data['daily'][set]['dt']
+    readdate1 = datetime.datetime.fromtimestamp(date1).strftime('%d-%m-%Y')
+    low1 = data['daily'][set]['temp']['min']
+    high1 = data['daily'][set]['temp']['max'] #I think this is right?
+    #descrip = data['daily'][0]['weather']['0']  This was being fickle, it's not use to a string i suppose
+    return readdate1, low1, high1
     
 
 zipcode = input("Input your zipcode: " ) #I didn't know you could shorthand this
@@ -37,6 +37,11 @@ lat, lon = Latlon(zipcode)
 
 print(f"Lat: {lat}, Lon: {lon}")
 
-date1, low1, high1 = weathergrab(lat, lon)
+date1, low1, high1 = weathergrab(lat, lon, 0) #Today's weather, date of today, high and low
+date2, low2, high2 = weathergrab(lat, lon, 1) #Tomorrow's weather, date of course, high and low
+date3, low3, high3 = weathergrab(lat, lon, 2) #The day after tomorrow, ditto as above
 
-print(f"Current date: {date1}, Low today: {low1} High today: {high1}")
+print(f"Date: {date1}, Low: {low1} High: {high1}")
+
+print(f"Date: {date2}, Low: {low2} High: {high2}")
+print(f"Date: {date3}, Low: {low3} High: {high3}")
