4ff2c66deda1f8398b5445b1ce4d1c9d1ab0d878
Ryan Flinchum <flinchur@my.erau.edu>
2023-02-12 13:28:19 -0500

    Added comment
    added comment

--- timewidget.py
+++ timewidget.py
@@ -12,7 +12,7 @@ class TimeWidget(QWidget):
         
         #Change font size (Font selection does not affect displayed font)
         font = QFont("Serif", 50)
-        font.Weight(1000)
+        font.Weight(1000) #Currently does not affect the Font weight
         #font.weight: 200
         
         # Create a label to display the current date and time
