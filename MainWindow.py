5ccfe0f75fe3e879e2708baaf3a638b1c65ec5c7
flinchur <118940946+flinchur@users.noreply.github.com>
2023-02-22 12:04:17 -0500

    Added single stock scrolling across top

--- MainWindow.py
+++ MainWindow.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import QMainWindow, QWidget
 
 # import custom subclasses
 from timewidget import TimeWidget
+from stockwidget import StockWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,7 +17,11 @@ class MainWindow(QMainWindow):
 
         # Create the time widget and add it to the main window
         self.time_widget = TimeWidget(self)
-        self.moveTimeWidget(100, 100) #moves the timewidget using the unique def below
+        self.moveTimeWidget(0, 100) #moves the timewidget using the unique def below
+
+        self.stock_widget = StockWidget(self)
+        #self.moveStockWidget(0, 0)
+
 
         #Create the x widget and add it to the main window
         
@@ -32,5 +37,8 @@ class MainWindow(QMainWindow):
     # moveTimeWidget is specifically for timewidget, each widget needs its own move def
     def moveTimeWidget(self, x, y):
         self.time_widget.move(x, y) # Moves the time widget to the specified position
+    # moveTimeWidget is specifically for timewidget, each widget needs its own move def
+    def moveStockWidget(self, x, y):
+        self.stock_widget.move(x, y) # Moves the time widget to the specified position
 
 
--- stockwidget.py
+++ stockwidget.py
@@ -2,19 +2,61 @@
 import json
 import pandas as pd
 from yahooquery import Ticker as yf
+from PyQt6.QtCore import QDateTime, QTimer, Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import QLabel
 
 #Assign Stocks of choice (Currently hardcoded)
 ticker = 'MSFT'
 
 #Print current ticker
 print('ticker: %s'%(ticker))
-yf_info = yf(ticker)
 
-#Grab Ticker Data
-data = yf_info.financial_data
+global olddata
+global xpos
+xpos = 0
 
-#Put data into dataframe
-data_formatted = pd.json_normalize(data)
+# This class uses the label widget of PyQt6
+class StockWidget(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # sets the font size, font, and color (** must be white to show on black mainwindow **)
+        self.setStyleSheet("font: 25pt Arial; color: white")
+
+        # sets the size of the label so all the text can be seen
+        self.setMinimumSize(250, 100) # Slowly increase until all text is visible
+        
+        # aligns the text to be in the center of the label
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter) #if the MinSize is too big, the text position will not match the move command
+                                                        #because it will be centering the text to the middle of the large label
+        
+        self.move(0,0)
 
 #Select desired data column and print
-print("Price: ",data_formatted[ticker + '.currentPrice'].iloc[-1])
+        self.timer = QTimer(self) # makes a timer
+        self.timer.timeout.connect(self.updateStock) #connects the timer to the showTime def (function)
+        self.timer.start(17) # 1000ms = 1 second
+        self.updateStock() # runs showTime initially to get rid of delay at program start
+
+    # function that "re-draws" the widget so it displays the time every new second
+    def updateStock(self):
+        global xpos
+        xpos = xpos + 1
+        
+        
+        yf_info = yf(ticker)
+        print(xpos)
+        
+        data = yf_info.financial_data
+        if xpos<=5:
+        #Grab Ticker Data
+            global olddata
+            olddata = data
+
+        #Put data into dataframe
+        data_formatted = pd.json_normalize(olddata)
+        text = str("MSFT: " + str(data_formatted[ticker + '.currentPrice'].iloc[-1]))
+        self.setText(text) 
+        self.move(xpos,0)
+        self.update()
