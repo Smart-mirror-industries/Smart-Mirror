8d80e707d8c299fbed8e4710511ae7ca4185bdfd
Sarah Harrington <sarah_harrington@me.com>
2023-02-22 14:43:20 -0500

    Added Map Widget, updated MainWindow

--- MainWindow.py
+++ MainWindow.py
@@ -1,36 +1,30 @@
 # import pyqt6 stuff
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
 
 
 # import custom subclasses
 from timewidget import TimeWidget
+from MapWidget import MapWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # Makes the main window and sets the background to black
         self.setWindowTitle('Smart Mirror')
-        self.setGeometry(100, 100, 400, 300) #Makes the MainWindow fullscreen
         self.setStyleSheet("background-color: black;")
 
-        # Create the time widget and add it to the main window
-        self.time_widget = TimeWidget(self)
-        self.moveTimeWidget(100, 100) #moves the timewidget using the unique def below
+        #set up grid widget layout
+        layout = QGridLayout()
 
-        #Create the x widget and add it to the main window
+        #add widgets to grid
+        layout.addWidget(TimeWidget(self), 0,0)
+        layout.addWidget(MapWidget(), 2,1)
+
+        #set the layout up to succesfully show
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
         
-
-
-    # use this def to add any widget to the mainwindow
-    # do "self.widgetname = classname(self)" in the mainwindow constructor above
-    def addWidget(self, widget):
-        self.central_widget.layout().addWidget(widget) #adds the inputted widget to mainwindow (self)
-
-    # To move specific widgets, create a def per widget using moveTimeWidget as a template:
-
-    # moveTimeWidget is specifically for timewidget, each widget needs its own move def
-    def moveTimeWidget(self, x, y):
-        self.time_widget.move(x, y) # Moves the time widget to the specified position
 
 
---
+++ MapWidget.py
@@ -0,0 +1,37 @@
+#code from https://zetcode.com/pyqt/qwebengineview/
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+
+
+class MapWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        #set up widget layout
+        vbox = QVBoxLayout(self)
+
+        #call webEngineView, loadPage function
+        self.webEngineView = QWebEngineView()
+        self.loadPage()
+
+        #add webEngineView to Layout
+        vbox.addWidget(self.webEngineView)
+        
+        #set size and show
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 350, 250)
+        self.show()
+
+    def loadPage(self):
+
+        with open('maps.html', 'r') as f: #this will call the maps API. DO NOT UNCOMMENT WHEN TESTING OTHER COMPONENETS
+        #with open('test.html', 'r') as f: #placeholder code
+            #read html file and load the page
+            html = f.read()
+            self.webEngineView.setHtml(html)
