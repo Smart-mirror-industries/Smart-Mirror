4fafe7cf4f47c39996b2f7bf80cc17bd6936108e
Ryan Flinchum <flinchur@my.erau.edu>
2023-03-02 12:27:38 -0500

    Uniform Scrolling
    Threading Setters & Getters to scroll all labels within the same instant

--- stockscroller.py
+++ stockscroller.py
@@ -7,7 +7,9 @@ from PyQt6.QtWidgets import QLabel
 
 from stockwidget import StockWidget
 from PyQt6.QtWidgets import QMainWindow, QWidget
-
+import threading
+global xpos
+xpos = 0
 
 class StockScroller(QWidget):
     def __init__(self,parent):
@@ -28,3 +30,28 @@ class StockScroller(QWidget):
         self.stock_widget2.move(-200,0)
         self.stock_widget3.move(100,0)
         #self.moveStockWidget(0, 0)
+        self.timer = QTimer(self) # makes a timer
+        self.timer.timeout.connect(self.scrollStocks) #connects the timer to the showTime def (function)
+        self.timer.start(8) # update every 8 ms = 120Hz
+        self.scrollStocks() # runs showTime initially to get rid of delay at program start
+        background_thread = threading.Thread(target=self.stock_widget.background_task)
+        background_thread.daemon = True
+        background_thread.start()
+        background_thread2 = threading.Thread(target=self.stock_widget2.background_task)
+        background_thread2.daemon = True
+        background_thread2.start()
+        background_thread3 = threading.Thread(target=self.stock_widget3.background_task)
+        background_thread3.daemon = True
+        background_thread3.start()
+        
+    def scrollStocks(self):
+        global xpos
+        if (xpos == 3000):
+            xpos = 0
+        
+        #print(oldtext)
+        self.stock_widget.setIndent(xpos)
+        self.stock_widget2.setIndent(xpos)
+        self.stock_widget3.setIndent(xpos)
+        
+        xpos = xpos + 2
--- stockwidget.py
+++ stockwidget.py
@@ -4,7 +4,8 @@ from yahooquery import Ticker as yf
 from PyQt6.QtCore import QDateTime, QTimer, Qt
 from PyQt6.QtGui import QPalette, QColor
 from PyQt6.QtWidgets import QLabel
-
+import threading
+import time
 #Assign Stocks of choice (Currently hardcoded)
 
 
@@ -14,8 +15,8 @@ from PyQt6.QtWidgets import QLabel
 #global ticker
 #ticker = 'IOVA'
 
-global xpos
-xpos = 0
+#global xpos
+#xpos = 0
 
 #global oldtext
 #oldtext = 'test'
@@ -29,7 +30,14 @@ class StockWidget(QLabel):
     #def setticker(self,stock):
     #    print(stock)
     #    self.ticker = stock
+    def setx(self,xpos):
+    #   print(stock)
+        self.xpos = xpos
 
+    def getx(self):
+    #   print(stock)
+        return self.xpos
+        
     def __init__(self, parent=None, ticker = 'IOVA'):
         super().__init__(parent)
         self.parent = parent
@@ -37,6 +45,7 @@ class StockWidget(QLabel):
         self.data = None
         self.data_formatted = {}
         self.oldtext = 'test'
+        self.xpos = 0
         self.updateStock()
 
         #print(str(self.getticker() + "AAAAA: " + str(data_formatted[self.getticker() + '.currentPrice'].iloc[-1])))
@@ -54,25 +63,69 @@ class StockWidget(QLabel):
         self.move(-500,0)           
 
 #Select desired data column and print
-        self.timer = QTimer(self) # makes a timer
-        self.timer.timeout.connect(self.updateStock) #connects the timer to the showTime def (function)
-        self.timer.start(8) # update every 8 ms = 120Hz
-        self.updateStock() # runs showTime initially to get rid of delay at program start
+        #self.timer = QTimer(self) # makes a timer
+        #self.timer.timeout.connect(self.updateStock) #connects the timer to the showTime def (function)
+        #self.timer.start(8) # update every 8 ms = 120Hz
+        #self.updateStock() # runs showTime initially to get rid of delay at program start
+        #while(1):
+        #    if(self.xpos == 0): 
+        #        pass
+        #        self.updateStock()
+            #else:
+               
+            #print(self.xpos)
+        #threading.Thread(target=self.thread_function()).start()
+    #def thread_function(self):
+        #while (1):
+        #    print('hi')
+        #    self.xpos = 1
+        #    if(self.xpos ==0):
+        #        self.updateStock()
+        #    else:
+        #        pass
+        # Create a new thread for the background task
+        
+        #print('testa')
+        #background_thread = threading.Thread(target=self.background_task())
+
+    # Set the thread to run in the background
+        #background_thread.daemon = True
+
+        # Start the thread
+
+        #background_thread.start()
+        #print('testb')
+
+
+    def background_task(self):
+        while True:
+            trigger = 0
+            if((self.getx() == 0)):
+                #print('test')
+                #self.updateStock()
+                #print('HERE')
+                #time.sleep(10)
+                trigger = 1
+            else:
+                self.setx(self.getx())
+                #print(self.getx)
+                #trigger = 0
+            
+            #print(self.getx())
+        # Your code for the background task goes here
+            pass
+
+    
 
     def updateStock(self):
         
-        global xpos
-        global oldtext
+        #global oldtext
 
-        if (xpos == 3000):
-            xpos = 0
-        #print(oldtext)
         
-        if(xpos == 0): 
-            yf_info = yf(self.getticker())     
-            #Put data into dataframe
-            data_formatted = pd.json_normalize(yf_info.financial_data)
-            self.oldtext = str(self.getticker() + ": " + str(data_formatted[self.getticker() + '.currentPrice'].iloc[-1]))
-        self.setText(self.oldtext) 
-        self.setIndent(xpos)
-        xpos = xpos + 1
+        yf_info = yf(self.getticker())     
+        #Put data into dataframe
+        data_formatted = pd.json_normalize(yf_info.financial_data)
+        #self.oldtext = 
+        self.setText(str(self.getticker() + ": " + str(data_formatted[self.getticker() + '.currentPrice'].iloc[-1]))) 
+        self.setIndent(self.getx())
+    
