d294403d1e5541d95278ffa4f14f9c807d447021
b0b00646 <h8jazz@gmail.com>
2023-03-07 15:28:04 -0500

    Created the start
    Weatherwidget.py is a thing now.

---
+++ Weatherwidget.py
@@ -0,0 +1,28 @@
+from PyQt6.QtCore import QDateTime, QTimer, Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import QLabel
+
+class weatherwidget(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # sets the font size, font, and color (** must be white to show on black mainwindow **)
+        self.setStyleSheet("font: 25pt Arial; color: white")
+
+        # sets the size of the label so all the text can be seen
+        self.setMinimumSize(450, 100) # Slowly increase until all text is visible
+        
+        # aligns the text to be in the center of the label
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter) #if the MinSize is too big, the text position will not match the move command
+                                                        #because it will be centering the text to the middle of the large label
+
+        # makes a timer to update the time every second
+        self.timer = QTimer(self) # makes a timer
+        self.timer.timeout.connect(self.showTime) #connects the timer to the showTime def (function)
+        self.timer.start(1000) # 1000ms = 1 second
+        self.Showreport() # runs showTime initially to get rid of delay at program start
+
+    # function that "re-draws" the widget so it displays the time every new second
+    def Showreport(self):
+        # TODO: make it grab the following days, and report back.  This needs to not exceed some limit with the api
+        pass
