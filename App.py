1ede02d28da4925cceca33f1c153fe254b9d69b2
Ryan Flinchum <flinchur@my.erau.edu>
2023-03-08 18:17:20 -0500

    Interfaced with Canvas API

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
 import stockwidget
+import canvasintegration
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
