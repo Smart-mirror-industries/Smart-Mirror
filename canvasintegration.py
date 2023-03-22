f53f80d5e5aa80ee422dbadd2d272212f6e300a8
Ryan Flinchum <flinchur@my.erau.edu>
2023-03-22 14:19:45 -0400

    Actual Canvas Integration with CLI output

--- canvasintegration.py
+++ canvasintegration.py
@@ -1,12 +1,24 @@
 import requests
+from ics import Calendar, Event
 
 #Canvas Export Link
 url = 'https://erau.instructure.com/feeds/calendars/user_3tuGhpDjBVOJLZ6wPiMKgXWa0vlyU1o6GoAMeycj.ics'
 
-r = requests.get(url) # create HTTP response object
-  
+r = requests.get(url) # Get Calendar File
+
+
+c = Calendar(r.text)  # Read calender file 
+
+c
+
+c.events
+
+
 # send a HTTP request to the server and save
 # the HTTP response in a response object called r
+for i in range(10):
+    e = list(c.timeline)[i]
+    print("Event '{}' started {}".format(e.name, e._end_time))
 with open("test.ics",'wb') as f:
   
     # Saving received content as a png file in
