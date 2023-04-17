3aa9c17fabca42b198db0bf26c5c7e914e9a1eb8
b0b00646 <h8jazz@gmail.com>
2023-04-16 20:05:48 -0400

    Update Weatherwidget.py

--- Weatherwidget.py
+++ Weatherwidget.py
@@ -48,7 +48,7 @@ class weatherwidget(QLabel):
         #text = (f"Date: {date1} \nLow: {low1} \nHigh: {high1}\n Description: {descrip1}\n")
         #text = (f"<html><body>Date: {date1}<br>" )
         #Current_temp = 
-        text = (f"<font size='2'><html><body>{date1}<br><font size='4'>{current1}<br><font size='2'>H: {high1} L: {low1}<br><img src='./weathersprites/{descrip1}.png'/><br><br>{date2}<br>H: {high2} L: {low2}<br><img src='./weathersprites/{descrip2}.png'/><br><br>{date3}<br>H: {high3} L: {low3}<br><img src='./weathersprites/{descrip3}.png'/></body></html>")
+        text = (f"<font size='2'><html><body>Today<br><font size='4'>{current1}<br><font size='2'>H: {high1} L: {low1}<br><img src='./weathersprites/{descrip1}.png'/><br><br>{date2}<br>H: {high2} L: {low2}<br><img src='./weathersprites/{descrip2}.png'/><br><br>{date3}<br>H: {high3} L: {low3}<br><img src='./weathersprites/{descrip3}.png'/></body></html>")
         #{date2}<br>Low: {low2}<br>High: {high2}<br><img src='./weathersprites/{descrip2}.png'/>
         #<br><br>{date3}<br>Low: {low3}<br>High: {high3}<br><img src='./weathersprites/{descrip3}.png'/>
         self.resize(pixmap1.width(), pixmap1.height())
