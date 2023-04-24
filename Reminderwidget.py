82daaab033372f69d6919ecd3fb18ef801f2458c
b0b00646 <h8jazz@gmail.com>
2023-04-23 20:31:24 -0400

    Reworked Reminderwidget
    Redid reminder widget  Still having no display issues

--- Reminderwidget.py
+++ Reminderwidget.py
@@ -1,43 +1,66 @@
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtWidgets import QApplication, QLineEdit, QMainWindow, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTextEdit
-
+import settings
 
 class reminderwidget(QWidget):
     def __init__(self, parent=None):
-        super(reminderwidget, self).__init__(parent)
+        super().__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+
+        self.add_button = QPushButton("Add")
+        self.delete_button = QPushButton("Delete")#Establish buttons
+
+        self.add_button.clicked.connect(self.add_textbox)
+        self.delete_button.clicked.connect(self.delete_textbox)
+
+        vbox1 = QVBoxLayout() #Holder for the text
+        vbox2 = QVBoxLayout() #To stack both of these into each other?
+
+        hbox1 = QHBoxLayout()#Holder for the buttons
+
+        hbox1.addWidget(self.add_button)
+        hbox1.addWidget(self.delete_button)
+
+        vbox2.addLayout(hbox1)
+        vbox2.addLayout(vbox1)
+
         
+
+        self.setLayout(vbox2)
+
+
         # set a layout for the boxes
-        self.textbox_layout = QVBoxLayout()
+        #self.textbox_layout = QVBoxLayout()
         
         #Set a layout for the buttons
-        self.button_layout = QHBoxLayout()
+        #self.button_layout = QHBoxLayout()
         
         # Create the "add" button
-        self.add_button = QPushButton("Add")
-        self.add_button.clicked.connect(self.add_textbox)
+        #self.add_button = QPushButton("Add")
+        #self.add_button.clicked.connect(self.add_textbox)
         
         # Create the "delete" button
-        self.delete_button = QPushButton("Delete")
-        self.delete_button.clicked.connect(self.delete_textbox)
+        #self.delete_button = QPushButton("Delete")
+        #self.delete_button.clicked.connect(self.delete_textbox)
         
         # Add the buttons to the button layout
-        self.button_layout.addWidget(self.add_button)
-        self.button_layout.addWidget(self.delete_button)
+        #self.button_layout.addWidget(self.add_button)
+        #self.button_layout.addWidget(self.delete_button)
         
         # Add the layouts to the main window
-        self.setLayout(self.textbox_layout)
-        self.layout().addLayout(self.button_layout)
+        #self.setLayout(self.textbox_layout)
+        #self.layout().addLayout(self.button_layout)
 
-
-        self.show()
         
     def add_textbox(self):#this creates text boxes
         # Create a new QLineEdit widget
         textbox = QLineEdit()
         
         # Add the text box to the layout
-        self.textbox_layout.addWidget(textbox)
+        self.vbox1.addWidget(textbox)
         
     def delete_textbox(self): #removes the last created text box, if there's no boxes it crashes
         
-        self.textbox_layout.takeAt(self.textbox_layout.count() - 1).widget().deleteLater()
+        self.vbox1.takeAt(self.textbox_layout.count() - 1).widget().deleteLater()
